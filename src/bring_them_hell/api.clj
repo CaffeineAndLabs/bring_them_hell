2982542ffeb287785b17c5333056a7391a85ff5e
Mickael Fortunato <morsi.morsicus@gmail.com>
2017-09-23 17:18:24 +0200

    refacto: api (namespace for schemas and utils)

--- src/bring_them_hell/api.clj
+++ src/bring_them_hell/api.clj
@@ -4,12 +4,9 @@
             [immuconf.config :as config]
             [cheshire.core :as json]
             [qbits.alia :as alia]
-            [schema.core :as s]))
-
-;; Helper
-(defn generate-random-uuid
-  []
-  (java.util.UUID/randomUUID))
+            [schema.core :as s]
+            [bring-them-hell.util :as util]
+            [bring-them-hell.repr :as repr]))
 
 ;; Configuration file
 (defn load-config-file
@@ -18,48 +15,16 @@
 
 (def my-config (load-config-file))
 
-;; Schemas for Configuration file
-(s/defschema CassandraDbDetails
-  {:ip s/Str
-   :keyspace s/Str})
-
-(s/defschema ConfigFile
-  {:cassandra CassandraDbDetails})
-
-;; Schemas
-(s/defschema RemoteNodeDetails
-  "Details for remote host candidate to Chaos"
-  {:hostname s/Str
-   :username s/Str})
-
-(s/defschema MonkeyTask
-  "A schema for a Monkey Task"
-  {(s/optional-key :uuid) s/Uuid
-   :name s/Str
-   ;; NOTE: Use LIST<frozen<node>> type on cassandra side. (Prefer to use SET but issue with it)
-   ;; ISSUE: a-clojure.lang.PersistentHashSet is use on clojure side to represent SET type (cassandra side)
-   ;; PersistentHashSet does not implements Sequential
-   ;; TODO: Dig in and check the deserialization of UDTValue (https://github.com/mpenet/alia/issues/45)
-   :nodes [RemoteNodeDetails]
-   :type (s/enum :reboot)})
-
-(s/defn generate-uuid-in-task-if-needed :- MonkeyTask
-  "Ensure that :uuid is present in MonkeyTask schema"
-  [monkey-task :- MonkeyTask]
-  (if (contains? monkey-task :uuid)
-    monkey-task
-    (assoc monkey-task :uuid (generate-random-uuid))))
-
 ;; Database things
 (s/defn insert-monkey-task-into-db
-  [db-details :- CassandraDbDetails
-   monkey-task :- MonkeyTask]
+  [db-details :- repr/CassandraDbDetails
+   monkey-task :- repr/MonkeyTask]
   (let [session (alia/connect (alia/cluster {:contact-points [(db-details :ip)]}))]
     (alia/execute session (str "USE " (db-details :keyspace)))
     (alia/execute session (str "INSERT INTO monkey_tasks JSON '" (json/generate-string monkey-task) "'"))))
 
 (s/defn get-all-monkey-tasks
-  [db-details :- CassandraDbDetails]
+  [db-details :- repr/CassandraDbDetails]
   (let [session (alia/connect (alia/cluster {:contact-points [(db-details :ip)]}))
         table "monkey_tasks"]
     (alia/execute session (str "USE " (db-details :keyspace)))
@@ -88,15 +53,15 @@
    (context "/api" []
             :tags ["api"]
             (GET "/monkey/tasks" []
-                 :return [MonkeyTask]
+                 :return [repr/MonkeyTask]
                  :summary "Return all tasks in the DB"
                  (ok (get-all-monkey-tasks (my-config :cassandra))))
 
             (POST "/monkey/task/new" []
-                  :return MonkeyTask
-                  :body [monkey-task MonkeyTask]
+                  :return repr/MonkeyTask
+                  :body [monkey-task repr/MonkeyTask]
                   :summary "Create your monkey task"
-                  (let [monkey-task (generate-uuid-in-task-if-needed monkey-task)]
+                  (let [monkey-task (util/generate-uuid-in-task-if-needed monkey-task)]
                     (insert-monkey-task-into-db (my-config :cassandra) monkey-task)
                     (ok monkey-task))))))
 
